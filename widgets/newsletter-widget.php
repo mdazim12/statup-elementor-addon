4669997e6daa2a3c7f0f177ad54f9dbba36dba08
Azim <azimuddin807@gmail.com>
2023-09-11 22:25:49 +0600

    Structure Done for Newslatter

--- widgets/newsletter-widget.php
+++ widgets/newsletter-widget.php
@@ -101,6 +101,84 @@ class Statup_mewslatter_Widget extends \Elementor\Widget_Base {
 	 */
 	protected function register_controls() {
 
+        $this->start_controls_section(
+			'banner_content',
+			[
+				'label' => esc_html__( 'Bannner Content', 'statup-extension' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+        $this->add_control(
+			'newslatter_title',
+			[
+				'label' => esc_html__( 'Title', 'statup-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Title here', 'statup-extension' ),
+                'label_block' =>true,
+                'default' => esc_html__( 'NEW FEATURES', 'statup-extension' ),
+			]
+		);
+
+        $this->add_control(
+			'newslatter_description',
+			[
+				'label' => esc_html__( 'Description', 'statup-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'placeholder' => esc_html__( 'Description here', 'statup-extension' ),
+                'label_block' =>true,
+                'default' => esc_html__( ' If you want to join the effort and help with the plugin, please.', 'statup-extension' ),
+			]
+		);
+
+        
+        $this->add_control(
+			'newslatter_btn',
+			[
+				'label' => esc_html__( 'Button', 'statup-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Button name', 'statup-extension' ),
+                'label_block' =>true,
+			]
+		);
+
+        $this->add_control(
+			'newslatter_btn_link',
+			[
+				'label' => esc_html__( 'Button Link', 'statup-extension' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'placeholder' => esc_html__( 'yourlink.com', 'statup-extension' ),
+                'label_block' =>true,
+			]
+		);
+
+        $this->end_controls_section();
+
+    
+        $this->end_controls_section();
+
+
+    
+?>
+
+                   <!-- <div class="row justify-content-center">
+                  <div class="col-xxl-10 col-xl-10 col-lg-11">
+                     <div class="features__offer text-center mt-20 wow fadeInUp" data-wow-delay=".9s" data-wow-duration="1s">
+                        <span>NEW FEATURES</span>
+                        <p> If you want to join the effort and help with the plugin, please <a href="contact.html">Let Us Know</a>.</p>
+                     </div>
+                  </div>
+               </div>               <div class="row justify-content-center">
+                  <div class="col-xxl-10 col-xl-10 col-lg-11">
+                     <div class="features__offer text-center mt-20 wow fadeInUp" data-wow-delay=".9s" data-wow-duration="1s">
+                        <span>NEW FEATURES</span>
+                        <p> If you want to join the effort and help with the plugin, please <a href="contact.html">Let Us Know</a>.</p>
+                     </div>
+                  </div>
+               </div> -->
+
+<?php
+
     }
 
 }
